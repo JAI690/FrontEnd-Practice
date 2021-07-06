691f598680bcbee1e89a304be8e08e71f7889f5d
Ele Cabrera <jai690@hotmail.com>
2021-07-05 21:54:46 -0500

    Incorporar el blog y SASS

---
+++ index.js
@@ -0,0 +1,6 @@
+// Crear proyecto de node
+// npm init -> agregar la configuracion
+// npm i sass -> instalar dependencia de sass desde 
+// Crear .gitignore y agregar texto de https://www.toptal.com/developers/gitignore/api/node
+// ejecutar comando -> ./node_modules/sass/sass.js --watch ./main.scss output.css
+// Compilador de sass a css -> https://beautifytools.com/scss-compiler.php
